07e150d9131a6b91db1d6b2e7859465b4f81326d
yunchiao <jojo050872@gmail.com>
2023-03-12 22:45:18 +0800

    Feature: Add the view of create new issue.

--- my-app/src/component/Filter.jsx
+++ my-app/src/component/Filter.jsx
@@ -1,16 +1,17 @@
-import { BiSearch } from "react-icons/bi";
+import { BiSearch, BiPlus } from "react-icons/bi";
 
 function Filter() {
     return (
-        <div className="bg-sky-600 w-full h-full p-6 flex justify-begin items-center gap-4">
+        <div className="bg-sky-600 w-full h-full p-6 flex justify-between items-center">
             <button className='bg-sky-700 hover:bg-sky-800 text-white w-12 h-8 rounded-md text-sm'>狀態</button>
             <button className='bg-sky-700 hover:bg-sky-800 text-white w-12 h-8 rounded-md text-sm'>新舊</button>
-            <div className='bg-sky-700 w-80 h-8 rounded-md flex justify-between'>
+            <div className='bg-sky-700 w-64 h-8 rounded-md flex justify-between'>
                 <input className='bg-transparent text-white w-64 h-8 truncate pl-3 focus:outline-none text-sm'
                        placeholder="這有搜尋可以用喔">
                 </input>
                 <button className='h-8 w-8 text-white hover:bg-sky-800 flex justify-center items-center rounded-md' title="搜尋"><BiSearch></BiSearch></button>
-            </div>          
+            </div>         
+            <button className='bg-sky-700 hover:bg-sky-800 text-white w-8 h-8 rounded-lg flex justify-center items-center'><BiPlus></BiPlus></button>
         </div>
     );
 }
