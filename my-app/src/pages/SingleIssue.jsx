378ca57977a7739198471dbc836fd5fd787a773e
yunchiao <jojo050872@gmail.com>
2023-03-19 22:18:14 +0800

    Fix: title would not be blur after typing the first char.

--- my-app/src/pages/SingleIssue.jsx
+++ my-app/src/pages/SingleIssue.jsx
@@ -15,10 +15,8 @@ function SingleIssues() {
     const { id } = useParams();
     const mdParser = new MarkdownIt(/* Markdown-it options */);
     const bodyRef = useRef(null);
-    const titleRef = useRef(null);
     const navigate = useNavigate();
     const [isBodyFocus, setIsBodyFocus] = useState(false);
-    const [isTitleFocus, setIsTitleFocus] = useState(false);
 
     /// Change the state of body when users type.
     const handleBodyChange = (text) => setBody(text);
@@ -32,12 +30,6 @@ function SingleIssues() {
     /// [isFocus] will be false if users click not focus on body content, and users can't edit it.
     const handleEditorBlur = () => setIsBodyFocus(false);
 
-    /// [isFocus] will be true if users click body content, and users can edit it.
-    const handleTitleFocus = () => setIsTitleFocus(true);        
-    
-    /// [isFocus] will be false if users click not focus on body content, and users can't edit it.
-    const handleTitleBlur = () => setIsTitleFocus(false);
-
     // Check if the body can be edited.
     useEffect(() => {
         if(isBodyFocus){
@@ -45,12 +37,6 @@ function SingleIssues() {
         }
     }, [isBodyFocus])
 
-    // Check if the body can be edited.
-    useEffect(() => {
-        if(isTitleFocus){
-            titleRef.current.focus();
-        }
-    }, [isTitleFocus])
     
     useEffect( () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -91,16 +77,19 @@ function SingleIssues() {
                 </div>
                 <div className="w-full min-h-screen bg-white p-10 text-black divide-y-2 divide-sky-200">
                     <div className='pb-2 text-3xl font-normal cursor-text'>
-                        <div onClick={handleTitleFocus} className={`w-full ${isTitleFocus || title.length===0?"hidden":""} truncate`}>{title}</div>
-                        <TextareaAutosize   onBlur={handleTitleBlur}
-                                            ref={titleRef}
-                                            value={title}
+                        <input  value={title}
+                                            onChange={e => handleTitleChange(e.target.value)}
+                                            placeholder={"請輸入標題"}
+                                            className={`border-none h-full w-full bg-transparent focus:outline-0 truncate`}>
+                                        
+                        </input>
+                        {/* <TextareaAutosize   value={title}
                                             onChange={e => handleTitleChange(e.target.value)}
                                             minRows={1}
                                             maxRows={1}
                                             placeholder={"請輸入標題"}
-                                            className={`border-none h-full bg-transparent focus:outline-0 truncate ${isTitleFocus || title.length === 0?"":"hidden"} `}
-                        ></TextareaAutosize>
+                                            className={`border-none h-full bg-transparent focus:outline-0 truncate`}
+                        ></TextareaAutosize> */}
                     </div>
                         <div onClick={handleEditorFocus}
                             onBlur={handleEditorBlur}
