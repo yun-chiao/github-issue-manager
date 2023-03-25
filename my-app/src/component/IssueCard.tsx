f09b0fd9879145ff3c1c7f379a3a0fadc2a73518
yunchiao <jojo050872@gmail.com>
2023-03-25 17:35:05 +0800

    Refactor: code styling.

--- my-app/src/component/IssueCard.tsx
+++ my-app/src/component/IssueCard.tsx
@@ -6,16 +6,16 @@ import MarkdownIt from 'markdown-it';
 import { useCookies } from 'react-cookie';
 import StateMenu from "./StateMenu";
 import React from "react";
-import { Issue }  from  "../type";
+import { Issue } from "../type";
 
-function IssueCard({issue}: {issue: Issue}): JSX.Element {
+function IssueCard({ issue }: { issue: Issue }): JSX.Element {
     const [cookies] = useCookies(['token', 'owner', 'repo']);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const mdParser = new MarkdownIt(/* Markdown-it options */);
 
     const toEditIssue = () => navigate(`/edit/${issue.number}`);
-    
+
 
     /// To close the issue when users click the trash icon.
     const deleteIssue = () => closeIssue(dispatch, issue.number, cookies['token'], cookies['owner'], cookies['repo']);
@@ -26,24 +26,24 @@ function IssueCard({issue}: {issue: Issue}): JSX.Element {
                 <StateMenu issue={issue}></StateMenu>
                 <div className="w-16 h-full flex items-center justify-between">
                     <button className="h-8 w-8 rounded-md hover:bg-slate-300 flex justify-center items-center"
-                            onClick={toEditIssue}
-                            title="Edit">
+                        onClick={toEditIssue}
+                        title="Edit">
                         <BiEdit />
                     </button>
                     <button className="h-8 w-8 rounded-md hover:bg-slate-300 flex justify-center items-center"
-                            onClick={deleteIssue}
-                            title="Close issue"
+                        onClick={deleteIssue}
+                        title="Close issue"
                     >
                         <BiTrashAlt />
                     </button>
                 </div>
             </div>
             <div className="w-full h-4/5 pl-8 pr-6 py-2">
-                <div  className="h-1/4 text-2xl truncate pt-1 hover:cursor-pointer hover:underline" onClick={toEditIssue}>{issue.title} </div>
-                <div  className="pt-2 h-3/4 break-all overflow-y-auto whitespace-pre-wrap hover:cursor-pointer hover:underline" 
-                      style={{background: "transparent"}} 
-                      onClick={toEditIssue}
-                      dangerouslySetInnerHTML={{ __html:  mdParser.render(issue.body) }} 
+                <div className="h-1/4 text-2xl truncate pt-1 hover:cursor-pointer hover:underline" onClick={toEditIssue}>{issue.title} </div>
+                <div className="pt-2 h-3/4 break-all overflow-y-auto card-scroll whitespace-pre-wrap hover:cursor-pointer hover:underline"
+                    style={{ background: "transparent" }}
+                    onClick={toEditIssue}
+                    dangerouslySetInnerHTML={{ __html: mdParser.render(issue.body) }}
                 />
             </div>
         </div>
