a41af349f1778ddfced66bf09458dc9bf0f3a5d2
yunchiao <jojo050872@gmail.com>
2023-03-25 16:18:25 +0800

    Refactor: Seperate the component state to useIssues hook.

--- my-app/src/hook/useIssues.tsx
+++ my-app/src/hook/useIssues.tsx
@@ -5,32 +5,37 @@ import { useCookies } from 'react-cookie';
 import { RootState } from '../type';
 import { toast } from "react-toastify";
 
-const useIssues = (page: number): { isLoading: boolean; } => {
+const useIssues = (): { isLoading: boolean; fetchMoreIssues: () => void } => {
   const [isLoading, setIsLoading] = useState(false);
   const [cookies] = useCookies(['token', 'owner', 'repo']);
+  const [page, setPage] = useState<number>(1);
   const dispatch = useDispatch();
   const filterState = useSelector((state: RootState) => state.filterStateReducer);
   const filterOrder = useSelector((state: RootState) => state.filterOrderReducer);
   const preSearchKey = useSelector((state: RootState) => state.filterKeywordReducer.keyword);
 
-  useEffect(() => {
-    if (page == 1) {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-      setIsLoading(true);
+  const fetchIssues = async (pageNum) => {
+    try {
+      await getIssues(dispatch, filterState, filterOrder, preSearchKey, cookies['token'], cookies['owner'], cookies['repo'], pageNum);
+    } catch (err) {
+      toast.error('Server error');
     }
-    const fetchIssues = async () => {
-      try {
-        await getIssues(dispatch, filterState, filterOrder, preSearchKey, cookies['token'], cookies['owner'], cookies['repo'], page);
-      } catch (err) {
-        toast.error('Server error');
-      }
-      setIsLoading(false);
-    };
+    setIsLoading(false);
+  };
 
-    fetchIssues();
-  }, [page, filterState, filterOrder, preSearchKey]);
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setIsLoading(true);
+    fetchIssues(1);
+    setPage(1);
+  }, [filterState, filterOrder, preSearchKey]);
 
-  return { isLoading };
+  const fetchMoreIssues = () => {
+    fetchIssues(page + 1);
+    setPage(page + 1);
+  };
+
+  return { isLoading, fetchMoreIssues };
 };
 
 export default useIssues;
--- my-app/src/pages/Issues.tsx
+++ my-app/src/pages/Issues.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import IssueCard from '../component/IssueCard';
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -12,48 +11,38 @@ import useIssues from '../hook/useIssues';
 function Issues(): JSX.Element {
     const issues = useSelector((state: RootState) => state.issuesReducer.issues);
     const hasMore = useSelector((state: RootState) => state.hasMoreReducer.hasMore);
-    const filterState = useSelector((state: RootState) => state.filterStateReducer);
-    const filterOrder = useSelector((state: RootState) => state.filterOrderReducer);
-    const preSearchKey = useSelector((state: RootState) => state.filterKeywordReducer.keyword);
-    const [page, setPage] = useState<number>(1);
-    const { isLoading } = useIssues(page);
-
-    useEffect(() => setPage(1), [filterState, filterOrder, preSearchKey])
-
-    const fetchMoreIssues = () => setPage(page + 1);
+    const { isLoading, fetchMoreIssues } = useIssues();
 
     return (
         <>
-            {isLoading ? (<div>Loading...</div>) : (
-                <div className="bg-sky-900 w-full h-full flex justify-center min-h-screen">
-                    <div className="bg-white w-list h-full flex flex-col items-center min-h-screen">
-                        <div className='bg-sky-600 w-full h-20 sticky top-0'>
-                            <Filter></Filter>
-                        </div>
-                        <InfiniteScroll
-                            style={{ "overflow": "hidden" }}
-                            className='divide-y'
-                            dataLength={issues.length}
-                            next={fetchMoreIssues}
-                            hasMore={hasMore}
-                            loader={
-                                <div className="text-center text-2xl my-4 h-10">
-                                    <h2>Loading</h2>
-                                </div>
-                            }
-                            endMessage={
-                                <div className="text-center text-2xl my-4 h-10">
-                                    <h2>No more issues!</h2>
-                                </div>
-                            }
-                        >
-                            {issues.map((issue) => (
-                                <IssueCard key={issue.number} issue={issue} />
-                            ))}
-                        </InfiniteScroll>
+            <div className="bg-sky-900 w-full h-full flex justify-center min-h-screen">
+                <div className="bg-white w-list h-full flex flex-col items-center min-h-screen">
+                    <div className='bg-sky-600 w-full h-20 sticky top-0'>
+                        <Filter></Filter>
                     </div>
+                    {isLoading ? (<div>Loading...</div>) : (
+                        <>
+                            <InfiniteScroll
+                                style={{ "overflow": "hidden" }}
+                                className='divide-y'
+                                dataLength={issues.length}
+                                next={fetchMoreIssues}
+                                hasMore={hasMore}
+                                loader={<div className="text-center text-2xl my-4 h-10">
+                                    <h2>Loading</h2>
+                                </div>}
+                                endMessage={<div className="text-center text-2xl my-4 h-10">
+                                    <h2>No more issues!</h2>
+                                </div>}
+                            >
+                                {issues.map((issue) => (
+                                    <IssueCard key={issue.number} issue={issue} />
+                                ))}
+                            </InfiniteScroll>
+                        </>
+                    )}
                 </div>
-            )}
+            </div>
         </>
     );
 }
